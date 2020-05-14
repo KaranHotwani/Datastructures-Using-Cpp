30ad5f49a4a0420d722ce5e858f1f4818317e6e3
Karan H <karanhotwani.kh@gmail.com>
2020-05-14 17:52:54 +0530

    Implemented insert at position and delete at position in doubly linked list

--- LinkedList/doublyLinkedList.cpp
+++ LinkedList/doublyLinkedList.cpp
@@ -24,6 +24,72 @@ void insertEnd(Node* &temp)
     temp1->next = temp;
     temp->prev = temp1;
 }
+
+void insertPosition(Node* &temp,int pos)
+{
+    //If temp is the first node then it will be head node
+    if(head==NULL)
+    {
+        head = temp;
+        return;
+    }
+    //store address of head in temp1 for traversing the linkedlist
+    Node* temp1 = head;
+    //handling the case of adding the new node at start
+    if(pos==1)
+    {
+        temp->next = head;
+        head->prev = temp;
+        head = temp;
+    }
+    //traversing and getting to one position before of actual position we have to add on
+    for(int i=0;i<pos-2;i++)
+    {
+        temp1=temp1->next;
+    }
+    //handling the case of adding the new node at end
+    if(temp1->next==NULL)
+    {
+        temp1->next=temp;
+        temp->prev=temp1;
+        return;
+    }
+    //handling the case of adding node anywhere in middle.All four links are getting reset
+    temp->next = temp1->next;
+    temp1->next->prev =  temp;
+    temp1->next = temp;
+    temp->prev =  temp1;
+}
+void deletePosition(int pos)
+{
+    Node* temp = head;
+    Node* temp1;
+    if(pos==1)
+    {
+        head = head->next;
+        head ->prev = NULL;
+        delete temp;
+        return;
+    }
+    for(int i=0;i<pos-2;i++)
+    {
+        temp = temp->next;
+    }
+    if(temp->next->next==NULL)
+    {
+        temp1 = temp->next;
+        temp1->prev = NULL;
+        temp->next = NULL;
+        delete temp1;
+        return;
+    }
+    temp1 = temp->next;
+    temp->next = temp1->next;
+    temp1->next->prev = temp;
+    temp1->next=NULL;
+    temp1->prev = NULL;
+    delete temp1;
+} 
 void printLinkedList(){
     Node* temp1 = head;
     while(temp1->next!=NULL)
@@ -43,8 +109,8 @@ int main()
         cout << "\nMenu Doubly LinkedList\n";
         cout << "1.Insert\n";
         cout << "2.Print\n";
-        // cout << "3.Print Node Count\n";
-        // cout << "4.Delete Node\n";
+        cout << "3.Insert at position\n";
+        cout << "4.Delete Node\n";
         // cout << "5.Reverse\n";
         cout << "0.Quit\n";
         cin >> choice;
@@ -69,6 +135,28 @@ int main()
             case 2:
             {
                 printLinkedList();
+            }
+            break;
+            case 3:Node n;
+                Node* temp = n;
+            {
+                Node * temp = new Node();
+                cout << "Insert Data\n";
+                cin >> temp->data;
+                temp->next = NULL;
+                temp->prev = NULL;
+                cout << "Insert Position\n";
+                int pos;
+                cin >> pos;
+                insertPosition(temp,pos);
+            }
+            break;
+            case 4:
+            {
+                cout << "Insert Position\n";
+                int pos;
+                cin >> pos;
+                deletePosition(pos);
             }
             break;
         }
